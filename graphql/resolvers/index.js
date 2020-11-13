3f3ef2022fb5f396b5c73e4b69a543ba3a3fbd00
Kevin Luo <kvn.luo@gmail.com>
2020-11-12 23:40:48 -0500

    add like and comment count to graphql typedefs

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -3,6 +3,14 @@ const usersResolvers = require('./users');
 const commentsResolvers = require('./comments');
 
 module.exports = {
+  // adding the Post type here allows us to track anytime that a query/mutation/subscription returns a post, and allows us to make modifications to that Post using the methods defined here
+  Post: {
+    likeCount(parent) {
+      console.log(parent);
+      return parent.likes.length;
+    },
+    commentCount: (parent) => parent.comments.length
+  },
   Query: {
     ...postsResolvers.Query
   },
