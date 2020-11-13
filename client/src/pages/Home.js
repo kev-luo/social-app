77d8b237b97f8647e9831ef6daaf16c4c67e866a
Kevin Luo <kvn.luo@gmail.com>
2020-11-13 11:45:25 -0500

    add new post transition effect

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react'
 import { useQuery } from '@apollo/client';
-import { Grid } from 'semantic-ui-react';
+import { Grid, Transition } from 'semantic-ui-react';
 
 import { AuthContext } from '../context/auth';
 import PostCard from '../components/PostCard';
@@ -26,13 +26,15 @@ export default function Home() {
         { loading ? (
           <h1>Loading posts...</h1>
         ) : (
-          data.getPosts && data.getPosts.map(post => {
-            return (
-              <Grid.Column key={ post.id } style={{ marginBottom: '20px'}}>
-                <PostCard post={post} />
-              </Grid.Column>
-            )
-          })
+          <Transition.Group>
+            {data.getPosts && data.getPosts.map(post => {
+              return (
+                <Grid.Column key={ post.id } style={{ marginBottom: '20px'}}>
+                  <PostCard post={post} />
+                </Grid.Column>
+              )
+            })}
+          </Transition.Group>
         )}
 
       </Grid.Row>
