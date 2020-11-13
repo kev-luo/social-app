16f68056b624757be46fdadaf85971eb52e99367
Kevin Luo <kvn.luo@gmail.com>
2020-11-13 10:50:30 -0500

    set up apollo-link-context

--- client/src/ApolloProvider.js
+++ client/src/ApolloProvider.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import App from './App';
 import { ApolloClient, InMemoryCache, createHttpLink, ApolloProvider } from '@apollo/client';
+import { setContext } from 'apollo-link-context';
 
 const httpLink = createHttpLink({
   uri: 'http://localhost:5000/'
 })
 
+// apollo client can get the jwt token from storage and add it as an authorization header whenever we send a request. basically acts as middleware by setting a context to the request and modifies the request and then forwards it to the http link
+const authLink = setContext(() => {
+  const token = localStorage.getItem('jwtToken');
+  return {
+    Authorization: token ? `Bearer ${ token }` : ''
+  }
+})
+
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache()
 })
 
