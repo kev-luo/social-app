97f357c4e8ff1ed1bafa8f8971851f2833ba4e87
Kevin Luo <kvn.luo@gmail.com>
2020-11-13 00:20:27 -0500

    menuBar

--- client/src/components/MenuBar.js
+++ client/src/components/MenuBar.js
@@ -1,28 +1,40 @@
 import React, { useState } from 'react';
 import { Menu } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
 
 export default function MenuBar() {
-  const [activeItem, setActiveItem] = useState('home');
-
+  const pathname = window.location.pathname;
+  const path = pathname === '/' ? 'home' : pathname.substring(1);
+  
+  const [activeItem, setActiveItem] = useState(path);
+  
   const handleItemClick = (e, { name }) => setActiveItem(name);
 
   return (
-    <Menu pointing secondary>
+    <Menu pointing secondary size="massive" color="red">
       <Menu.Item
         name='home'
         active={activeItem === 'home'}
         onClick={handleItemClick}
+        // in semantic ui react, 'as' allows this to behave as a different component
+        as={ Link }
+        // this is a prop that gets passed to the Link
+        to="/"
       />
       <Menu.Menu position='right'>
         <Menu.Item
           name='login'
           active={activeItem === 'login'}
           onClick={handleItemClick}
+          as={ Link }
+          to="/login"
         />
         <Menu.Item
           name='register'
           active={activeItem === 'register'}
           onClick={handleItemClick}
+          as={ Link }
+          to="/register"
         />
       </Menu.Menu>
     </Menu>
